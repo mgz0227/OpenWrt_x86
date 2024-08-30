bd2ae41c3d72a4e87b138b0ec222a4f3f3ede900
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2024-08-30 20:03:46 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -9,9 +9,10 @@ git_clone_path master https://github.com/coolsnowwolf/lede target/linux/x86/file
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/base-files/etc/board.d/02_network -P target/linux/x86/base-files/etc/board.d/
 
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/64/config-6.6 -P target/linux/x86/64/
-wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
 
-rm -rf target/linux/generic/pending-6.6/601-udp-fix-receiving-fraglist-GSO_packets.patch
+#wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
+#rm -rf target/linux/generic/pending-6.6/601-udp-fix-receiving-fraglist-GSO_packets.patch
+
 sed -i 's/kmod-r8169/kmod-r8168/' target/linux/x86/image/64.mk
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdhci usbutils pciutils lm-sensors-detect kmod-alx kmod-vmxnet3 kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-r8101 kmod-r8125 kmod-r8126 kmod-8139cp kmod-8139too kmod-i40e kmod-drm-i915 kmod-drm-amdgpu kmod-mlx4-core kmod-mlx5-core fdisk lsblk kmod-phy-broadcom kmod-ixgbevf/' target/linux/x86/Makefile
