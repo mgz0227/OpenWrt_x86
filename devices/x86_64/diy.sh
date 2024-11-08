c0eca57ee7b81024cc7135d4524ee5e855470561
miaogongzi <miaogongzi0227@gmail.com>
2024-11-09 01:07:45 +0800

    update

---
+++ devices/x86_64/diy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
+#bash $SHELL_FOLDER/../common/kernel_6.6.sh
+
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/x86/files target/linux/x86/patches-6.6
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/base-files/etc/board.d/02_network -P target/linux/x86/base-files/etc/board.d/
+
+
+#wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
+#rm -rf target/linux/generic/backport-6.6/780-24-v6.12-r8169-avoid-unsolicited-interrupts.patch
+
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/64/config-6.6 -P target/linux/x86/64/
+
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/firmware/linux-firmware/intel.mk -P package/firmware/linux-firmware/
+
+sed -i 's/kmod-r8169/kmod-r8168/' target/linux/x86/image/64.mk
+
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdhci usbutils pciutils lm-sensors-detect kmod-atlantic kmod-vmxnet3 kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-r8101 kmod-r8125 kmod-r8126 kmod-8139cp kmod-8139too kmod-i40e kmod-drm-i915 kmod-drm-amdgpu kmod-mlx4-core kmod-mlx5-core fdisk lsblk kmod-phy-broadcom kmod-ixgbevf/' target/linux/x86/Makefile
+
+mv -f tmp/r81* feeds/miaogongzi/
+
+sed -i 's/256/1024/g' target/linux/x86/image/Makefile
+
+
