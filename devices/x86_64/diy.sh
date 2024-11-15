f51ead9ff643608db4f724f9696c4d4c1b854bb7
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2024-11-15 17:56:17 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -21,7 +21,7 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/firmw
 
 #wget -N https://raw.githubusercontent.com/openwrt/openwrt/openwrt-24.10/package/kernel/mac80211/patches/build/236-fix-genlmsg_multicast_allns-build-error-on-kernel-6.6.59.patch -P package/kernel/mac80211/patches/build/
 
-#wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
+wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
 
 #结束
 
