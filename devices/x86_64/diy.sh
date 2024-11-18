6f0aa54695f6d2aaf1bb3d47bf8cb3281af32db4
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2024-11-19 06:41:37 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -19,12 +19,12 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/firmw
 #rm -rf target/linux/generic/backport-6.6/780-24-v6.12-r8169-avoid-unsolicited-interrupts.patch
 #rm -rf target/linux/generic/backport-6.6/409-mtd-spi-nor-winbond-fix-w25q128-regression.patch
 
-rm -rf package/kernel/mac80211
-rm -rf feeds/routing/batman-adv
-rm -rf package/kernel/ath10k-ct
-rm -rf package/kernel/mt76
-rm -rf package/firmware/ath10k-ct-firmware
-rm -rf package/kernel/rtl8812au-ct
+#rm -rf package/kernel/mac80211
+#rm -rf feeds/routing/batman-adv
+#rm -rf package/kernel/ath10k-ct
+#rm -rf package/kernel/mt76
+#rm -rf package/firmware/ath10k-ct-firmware
+#rm -rf package/kernel/rtl8812au-ct
 
 #wget -N https://raw.githubusercontent.com/openwrt/openwrt/openwrt-24.10/package/kernel/mac80211/patches/build/236-fix-genlmsg_multicast_allns-build-error-on-kernel-6.6.59.patch -P package/kernel/mac80211/patches/build/
 
