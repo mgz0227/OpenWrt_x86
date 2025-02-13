2fef6b1cadf41f6a4dfaa5e9540f0d7c4d603930
miaogongzi <miaogongzi0227@gmail.com>
2025-02-13 22:31:49 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -13,7 +13,7 @@ sed -i '/	refresh_config();/d' scripts/feeds
 ./scripts/feeds install -a -p miaogongzi -f
 ./scripts/feeds install -a
 
-sed -i "s/%C\"/%C by miaogongzi\"" package/base-files/files/etc/*release
+sed --follow-symlinks -i "s#%C\"#%C by miaogongzi\"#" package/base-files/files/etc/os-release
 sed -i -e '$a /etc/bench.log' \
         -e '/\/etc\/profile/d' \
         -e '/\/etc\/shinit/d' \
