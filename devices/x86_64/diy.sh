bbd66453d823c15444687f08ad7a2107e5522d50
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2025-02-27 23:07:24 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -14,6 +14,7 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/
 
 #rm -rf target/linux/generic/hack-6.6/767-net-phy-realtek-add-led-link-select-for-RTL8221.patch
 rm -rf target/linux/generic/backport-6.6/819-v6.8-0002-nvmem-Create-a-header-for-internal-sharing.patch
+rm -rf target/linux/generic/backport-6.6/819-v6.8-0003-nvmem-Simplify-the-add_cells-hook.patch
 wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
 
 #结束
