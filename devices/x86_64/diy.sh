6319e3c1b6631fe9bfb3a27fa8f1c9e78b041e21
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2025-03-08 04:20:59 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -14,17 +14,6 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/
 
 #rm -rf target/linux/generic/hack-6.6/767-net-phy-realtek-add-led-link-select-for-RTL8221.patch
 
-
-rm -rf target/linux/generic/backport-6.6/819-v6.8-0002-nvmem-Create-a-header-for-internal-sharing.patch
-rm -rf target/linux/generic/backport-6.6/819-v6.8-0003-nvmem-Simplify-the-add_cells-hook.patch
-rm -rf target/linux/generic/backport-6.6/819-v6.8-0004-nvmem-Move-and-rename-fixup_cell_info.patch
-#rm -rf target/linux/generic/backport-6.6/820-v6.9-00*
-#rm -rf target/linux/generic/backport-6.6/821-v6.10-00*
-#rm -rf target/linux/generic/backport-6.6/819-v6.11-00*
-#rm -rf target/linux/generic/backport-6.6/820-v6.11-00*
-#rm -rf target/linux/generic/backport-6.6/821-v6.11-00*
-#rm -rf target/linux/generic/backport-6.6/822-v6.11-00*
-#rm -rf target/linux/generic/pending-6.6/804-nvmem*
 wget -N https://raw.githubusercontent.com/mgz0227/openwrt/main/include/kernel-6.6 -P include/
 
 #结束
