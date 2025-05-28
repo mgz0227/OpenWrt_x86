0bafe1c3c2c37390e333cedb9e21fba651b1db85
gongzi miao <miaogongzi0227@gmail.com>
2025-05-29 01:41:39 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -10,10 +10,10 @@ git reset --hard origin/master
 cp -rf --parents target/linux package/kernel  package/network package/boot package/firmware package/network package/libs config/Config-kernel.in ../
 cd -
 
-cd feeds/packages
-rm -rf net/xtables-addons
+#cd feeds/packages
+#rm -rf net/xtables-addons
 
-cd ../../
+#cd ../../
 mv feeds/miaogongzi/xtables-addons feeds/packages/net/
 cd package
 rm -rf devel/kselftests-bpf devel/perf kernel/mt76
