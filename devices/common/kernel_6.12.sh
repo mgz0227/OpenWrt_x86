b2a0ca62343c107a1ec8243109decba9c8925f00
miaogongzi <miaogongzi0227@gmail.com>
2025-08-09 09:59:15 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -11,8 +11,8 @@ cp -rf --parents target/linux package/boot package/firmware package/kernel packa
 cd -
 
 cd feeds/packages
-rm -rf net/xtables-addons net/strongswan utils/coremark lang/golang net/openvpn net/openvswitch
-git_clone_path master https://github.com/openwrt/packages net/xtables-addons net/strongswan lang/golang net/openvpn net/openvswitch
+rm -rf net/xtables-addons net/strongswan utils/coremark lang/golang
+git_clone_path master https://github.com/openwrt/packages net/xtables-addons net/strongswan lang/golang
 cd ../../
 
 
