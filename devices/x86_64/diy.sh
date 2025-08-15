0b7a5e9e112d026e8f83cf972a6c3f661fe044e4
gongzi miao <miaogongzi0227@gmail.com>
2025-08-15 19:34:38 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -17,8 +17,8 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/
 #rm -rf target/linux/generic/pending-6.12/680-net-fix-TCP-UDP-fraglist-GRO.patch
 #Wget -N https://github.com/mgz0227/packages/raw/refs/heads/Rust/lang/rust/patches/002-bypass-github-actions-ci.patch -P feeds/lang/rust/patches/
 #以下不能动
-#wget -N https://raw.githubusercontent.com/mgz0227/openwrt/refs/heads/main/target/linux/generic/kernel-6.12 -P include/
-wget -N https://raw.githubusercontent.com/openwrt/openwrt/2be61fbc344d7b00039ae5800b3419b6ac79075f/target/linux/generic/kernel-6.12 -P include/
+wget -N https://raw.githubusercontent.com/mgz0227/openwrt/refs/heads/main/target/linux/generic/kernel-6.12 -P include/
+#wget -N https://raw.githubusercontent.com/openwrt/openwrt/2be61fbc344d7b00039ae5800b3419b6ac79075f/target/linux/generic/kernel-6.12 -P include/
 
 #结束
 
