5a12c3ed0ac87108e79ab39702a7afb317965a23
gongzi miao <miaogongzi0227@gmail.com>
2025-08-22 10:34:11 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -11,7 +11,7 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/
 
 #内核升级模块
 #开始
-wget -N http://cdn.miaogongzi.cc/patch/6.12.43.patch -P target/linux/x86/patches-6.12
+wget -N http://cdn.miaogongzi.cc/patch/6.12.43-1.patch -P target/linux/x86/patches-6.12
 rm -rf target/linux/generic/backport-6.12/612-11-v6.17-net-dsa-b53-prevent-GMII_PORT_OVERRIDE_CTRL-access-on-BCM5325.patch
 rm -rf target/linux/generic/backport-6.12/612-09-v6.17-net-dsa-b53-prevent-DIS_LEARNING-access-on-BCM5325.patch
 rm -rf target/linux/generic/backport-6.12/612-08-v6.17-net-dsa-b53-fix-IP_MULTICAST_CTRL-on-BCM5325.patch
