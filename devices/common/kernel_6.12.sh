c14e877e244ea85c57d57c5db85d898ad8fc6c3a
miaogongzi <miaogongzi0227@gmail.com>
2025-09-02 00:06:49 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -16,7 +16,7 @@ git_clone_path master https://github.com/openwrt/packages net/xtables-addons net
 cd ../../
 
 
-cd package
-rm -rf devel/kselftests-bpf 
+#cd package
+#rm -rf devel/kselftests-bpf 
 #devel/perf
-cd ../
+#cd ../
