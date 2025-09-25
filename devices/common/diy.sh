dfd58fbe06a1bcd1ae39a15dd92bdadfbae3e50e
gongzi miao <miaogongzi0227@gmail.com>
2025-09-25 18:32:45 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -2,6 +2,11 @@
 #=================================================
 shopt -s extglob
 
+#临时切换mirror
+sed -i 's#https://git.openwrt.org/feed/packages.git#https://github.com/openwrt/packages.git#g' feeds.conf.default
+sed -i 's#https://git.openwrt.org/project/luci.git#https://github.com/openwrt/luci.git#g' feeds.conf.default
+sed -i 's#https://git.openwrt.org/feed/routing.git#https://github.com/openwrt/routing.git#g' feeds.conf.default
+#
 
 
 sed -i '$a src-git miaogongzi https://github.com/mgz0227/OP-Packages.git;master' feeds.conf.default
