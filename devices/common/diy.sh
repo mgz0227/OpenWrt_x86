019c66bd05121e3dc246a92df91323076b6ef3f6
gongzi miao <miaogongzi0227@gmail.com>
2026-01-25 23:35:08 +0800

    “无”firewall

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -47,7 +47,7 @@ echo "$(date +"%s")" >version.date
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
 sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-advancedplus luci-app-firewall luci-app-package-manager \
 luci-app-wizard luci-base luci-compat luci-lib-ipkg luci-lib-fs luci-app-log-viewer \
-luci-app-argon-config luci-app-ddns-go luci-app-openclash tcpdump-mini open-vm-tools \
+luci-app-argon-config luci-app-ddns-go luci-app-openclash luci-app-adblock tcpdump-mini open-vm-tools \
 coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan luci-app-filemanager /" include/target.mk
 
 sed -i "s/procd-ujail//" include/target.mk
