5af6e82dd0a45249d7fd9e43a7ea11f7ac6d61ca
mgz0227 <miaogongzi0227@gmail.com>
2026-03-11 23:03:21 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -79,7 +79,10 @@ sed -i 's/max_requests 3/max_requests 20/g' package/network/services/uhttpd/file
 sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" target/linux/*/base-files/etc/inittab
 
 date=`date +%m.%d.%Y`
-sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
+sed -i \
+  -e "/\(# \)\?REVISION:=/c\REVISION:=$date" \
+  -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' \
+  include/version.mk
 
 sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
 sed -i 's#20) \* 1000#60) \* 1000#g' feeds/luci/modules/luci-base/htdocs/luci-static/resources/rpc.js
