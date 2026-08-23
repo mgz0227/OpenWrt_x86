07c5312abc521ffc34cb41c165dd9294ed30e2c9
gongzi miao <miaogongzi0227@gmail.com>
2026-08-24 00:31:07 +0800

    Update kernel_6.18.sh

--- devices/common/kernel_6.18.sh
+++ devices/common/kernel_6.18.sh
@@ -13,9 +13,9 @@ cd -
 
 
 cd feeds/packages
-rm -rf net/xtables-addons net/jool kernel/v4l2loopback kernel/ovpn-dco libs/libpfring libs/libmariadb lang/python
+rm -rf net/xtables-addons net/jool kernel/v4l2loopback kernel/ovpn-dco libs/libpfring libs/libmariadb lang/python/python3
 
-git_clone_path master https://github.com/openwrt/packages net/jool kernel/v4l2loopback libs/libpfring net/xtables-addons libs/libmariadb lang/python kernel/ovpn-dco 
+git_clone_path master https://github.com/openwrt/packages net/jool kernel/v4l2loopback libs/libpfring net/xtables-addons libs/libmariadb lang/python/python3 kernel/ovpn-dco 
 
 cd ../../
 
