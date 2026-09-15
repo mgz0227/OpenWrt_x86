d0225fbff2dfd1ffae73fac59b0ee5483a072705
gongzi miao <miaogongzi0227@gmail.com>
2026-09-15 17:27:26 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -16,7 +16,6 @@ rm -rf package/base-files/files/etc/profile.d/apk-cheatsheet.sh
 rm -rf target/linux/generic
 git_clone_path 18 https://github.com/graysky2/openwrt target/linux/generic target/linux/generic
 
-rm -rf target/linux/generic/hack-6.18/200-tools_portability.patch
 
 #以下不能动
 
